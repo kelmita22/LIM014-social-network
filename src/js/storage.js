18a311463af55392eb26efec9b5368aafa44e27a
Erika <erikagonza2013@gmail.com>
2021-05-04 18:57:05 -0500

    Comentando funciones para una mejor identificacion

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,5 +1,5 @@
-// ----------------------------Guardar foto STORAGE ------------------------------------
-export const sendImgToStorage = (file, location) => {
+// Función para guardar foto del usario
+export const imgStorage = (file, location) => {
   const storageRef = firebase.storage().ref(`${location}/${file.name}`);
   return storageRef.put(file);
 };
