b26177d6b037765b18b91a45f34fe77c4ed052ca
Erika <erikagonza2013@gmail.com>
2021-05-04 18:57:38 -0500

    Creación de vista 404 error

--- src/view/404.js
+++ src/view/404.js
@@ -1,8 +1,17 @@
 export default () => {
   const viewDifferent = `
-          <h2>404</h2>
-          <h1>Página no encontrada</h1>
-          <p>El archivo especificado no se encontró en este sitio web. Por favor, compruebe la URL para errores y vuelva a intentarlo.</p>
+  <div class="body404">
+  <section id="error-page">
+    <section class="content">
+      <h2 class="header">404</h2>
+        <h4>Opps! Página no encontrada</h4>
+          <p>Lo sentimos, la ruta ingresada no es correcta, presiona el botón de inicio para visitar nuestra página </p>
+      <section class="bottons">
+        <a href="#/">Ir al Inicio</a>
+      </section>
+    </section>
+  </section>
+</section>
         `;
 
   const divElemt = document.createElement('div');
