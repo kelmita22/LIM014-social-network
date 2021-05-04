0b7b9214c8473b7979b9a8cf35d1f962789f75b1
Erika <erikagonza2013@gmail.com>
2021-05-04 18:55:53 -0500

    Comentando funciones para identificar que hace cada una

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,29 +1,29 @@
-// ---------------------------------------------SIGN IN----------------------------------------
+// Funciones para iniciar sesión(correo, gmail, facebook)
 export const signIn = (email, password) => {
   const auth = firebase.auth();
   return auth.signInWithEmailAndPassword(email, password);
 };
-export const signInForGoogle = () => {
+export const loginGoogle = () => {
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
   return auth.signInWithPopup(provider);
 };
-export const signInForFacebook = () => {
+export const loginFacebook = () => {
   const provider = new firebase.auth.FacebookAuthProvider();
   return firebase.auth().signInWithPopup(provider);
 };
-// ---------------------------------------------SIGN UP---------------------------------------
-export const createUser = (email, password) => {
+// Funcion para crear usuario nuevo
+export const signUp = (email, password) => {
   const auth = firebase.auth();
   return auth.createUserWithEmailAndPassword(email, password);
 };
-// Send email to verify created account
-export const sendEmail = () => {
+// Enviar correo para verificación
+export const emailVerification = () => {
   const user = firebase.auth().currentUser;
   return user.sendEmailVerification();
 };
-// -------------------------------------------SIGN OUT----------------------------------------
+// Función para cerrar sesión
 export const signOut = () => firebase.auth().signOut();
 
-// --------------------------------------GET CURRENT USER------------------------------------
+// Administración de usuarios Firebase
 export const currentUser = () => firebase.auth().currentUser;
