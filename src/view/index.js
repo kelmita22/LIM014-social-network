62eb17ed53abd813ffdc0c1a32648eac4fcb8c30
Erika <erikagonza2013@gmail.com>
2021-05-05 12:52:08 -0500

    Quitando el componente header que ya no sera necesario

--- src/view/index.js
+++ src/view/index.js
@@ -1,6 +1,5 @@
 import Login from './login.js';
 import Check from './check.js';
-import Header from './header.js';
 import viewHome from './home.js';
 import Profile from './profile.js';
 import ViewDifferent from './404.js';
@@ -8,7 +7,6 @@ import ViewDifferent from './404.js';
 const components = {
   login: Login,
   check: Check,
-  header: Header,
   home: viewHome,
   profile: Profile,
   viewDifferent: ViewDifferent,
